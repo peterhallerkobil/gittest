128d5330a42868b32d6d8fe11002b8423b8b79e8
Peter Haller <peter.haller@kobil.com>
2014-10-31 12:45:54 +0100

    push firstbranch to master server

--- src/gittest/Main.java
+++ src/gittest/Main.java
@@ -1,15 +1,36 @@
 package gittest;
 
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Main extends JFrame
 {
 	public Main()
 	{
 		setSize(800,600);
+		
+		Container content = getContentPane();
+		content.setLayout(new FlowLayout());
+		JButton but = new JButton("show dialog");
+		but.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent arg0)
+			{
+				showBranchDialog();
+				
+			}
+		});
+		content.add(but);
+		
+		
 		setLocation(400,400);
 		setVisible(true);
 		addWindowListener(new WindowAdapter() {
@@ -22,6 +43,10 @@ public class Main extends JFrame
 			}
 		});
 	
+	}
+	
+	private void showBranchDialog() {
+		JOptionPane.showMessageDialog(this,"This is the first branch");
 	}
 	
 	public static void main(String[] args) {
